ce807aec277c29facbc1ce7aa7ee69aca8c2e51d
mgrakib <mgrakibbd@gmail.comgit config --global user.email mgrakibbd@gmail.com>
2026-01-26 04:30:59 +0600

    vercel

--- index.js
+++ index.js
@@ -559,7 +559,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! this is new");
 });
 
 app.listen(port, () => {
